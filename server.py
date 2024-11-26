42e444530ac4bbd7c5bcb55be4b28d7876c69159
Maryam Abdulle <114389244+Maryamabdulle@users.noreply.github.com>
2024-11-26 18:11:51 +0300

    Update server.py

--- server.py
+++ server.py
@@ -934,6 +934,4 @@ def handle_email():
 
 
 if __name__ == "__main__":
-    # DebugToolbarExtension(app)
-    connect_to_db(app)
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
